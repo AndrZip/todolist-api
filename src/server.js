d8f63922b1f34bd86457606cf4de1398f531a3a8
André Lisboa <andrewemail52@gmail.com>
2023-10-05 23:04:44 -0300

    porta padrão alterada

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`[LOG] - Conexão Servidor: OK | Porta: ${PORT}`));
